d99295034c584ade8632314dd00c31d44dea51ec
Andryteck <772316andrew@gmail.com>
2020-08-20 18:25:34 +0300

    feat: add hashrouter

--- build/precache-manifest.73b8320302c8046f4780a39fab0cb9a1.js
+++ build/precache-manifest.73b8320302c8046f4780a39fab0cb9a1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bf6ad22a7c960c9256011a902355e557",
+    "revision": "76f875f937027f9f4f33b7af21be2d1c",
     "url": "/Todolist/index.html"
   },
   {
-    "revision": "4668f1c07489f7bc1be0",
+    "revision": "0d80391555c06e55253b",
     "url": "/Todolist/static/css/main.aa207b39.chunk.css"
   },
   {
-    "revision": "d183aa1ccdf938b26fca",
-    "url": "/Todolist/static/js/2.1b2df70b.chunk.js"
+    "revision": "963dd0d8398b084fbce3",
+    "url": "/Todolist/static/js/2.59638919.chunk.js"
   },
   {
     "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
-    "url": "/Todolist/static/js/2.1b2df70b.chunk.js.LICENSE.txt"
+    "url": "/Todolist/static/js/2.59638919.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4668f1c07489f7bc1be0",
-    "url": "/Todolist/static/js/main.1bcf4dfe.chunk.js"
+    "revision": "0d80391555c06e55253b",
+    "url": "/Todolist/static/js/main.a051a99b.chunk.js"
   },
   {
     "revision": "8c9ba947015c2dd63600",
