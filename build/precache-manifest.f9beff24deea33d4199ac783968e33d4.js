6b5d75c8141c46df1e120142b698dfec56a8f7fb
Andryteck <772316andrew@gmail.com>
2020-08-20 18:22:40 +0300

    feat: add hashrouter

---
+++ build/precache-manifest.f9beff24deea33d4199ac783968e33d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6ad22a7c960c9256011a902355e557",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "4668f1c07489f7bc1be0",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "d183aa1ccdf938b26fca",
+    "url": "/Todolist/static/js/2.1b2df70b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.1b2df70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4668f1c07489f7bc1be0",
+    "url": "/Todolist/static/js/main.1bcf4dfe.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
